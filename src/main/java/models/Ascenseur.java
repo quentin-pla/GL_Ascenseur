522e310a88993139d5bca28e2fc9be27d2bed06a
Quentin Pla <quentin-pla@hotmail.fr>
2020-09-18 18:10:47 +0200

    Liaison des boutons à l'ascenseur

--- src/main/java/models/Ascenseur.java
+++ src/main/java/models/Ascenseur.java
@@ -1,5 +1,10 @@
 package models;
 
+import models.commandes.utilisateur.exterieur.AppelDescendre;
+import models.commandes.utilisateur.exterieur.AppelMonter;
+import models.commandes.utilisateur.interieur.DemandeArretUrgence;
+import models.commandes.utilisateur.interieur.DeplacementNiveau;
+
 import java.util.ArrayList;
 
 public class Ascenseur {
@@ -27,5 +32,40 @@ public class Ascenseur {
         this.engine = new Moteur(levels, defaultLevel);
         this.externalButtons = new ArrayList<>();
         this.internalButtons = new ArrayList<>();
+        linkButtons();
+    }
+
+    /**
+     * Relier les boutons aux commandes
+     */
+    private void linkButtons() {
+        //Ajout des boutons externes
+        externalButtons.add(new Bouton(AppelDescendre.getInstance()));
+        externalButtons.add(new Bouton(AppelMonter.getInstance()));
+        //Ajout des boutons internes
+        internalButtons.add(new Bouton(DemandeArretUrgence.getInstance()));
+        internalButtons.add(new Bouton(DeplacementNiveau.getInstance()));
+    }
+
+    /*** GETTERS ***/
+
+    public Moteur getEngine() {
+        return engine;
+    }
+
+    public ArrayList<Bouton> getExternalButtons() {
+        return externalButtons;
+    }
+
+    public ArrayList<Bouton> getInternalButtons() {
+        return internalButtons;
+    }
+
+    public static void main(String[] args) {
+        Ascenseur ascenseur = new Ascenseur(4,0);
+        ascenseur.getExternalButtons().get(0).click();
+        ascenseur.getExternalButtons().get(1).click();
+        ascenseur.getInternalButtons().get(0).click();
+        ascenseur.getInternalButtons().get(1).click();
     }
 }
