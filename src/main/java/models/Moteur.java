6707fb016fb9186402f417ee9eea73ebe22b39e3
Quentin Pla <quentin-pla@hotmail.fr>
2020-09-18 18:04:59 +0200

    Implémentation classe Moteur

---
+++ src/main/java/models/Moteur.java
@@ -0,0 +1,79 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Moteur {
+
+    /**
+     * Nombre de niveaux (constant)
+     */
+    private final int levels;
+
+    /**
+     * Niveau actuel
+     */
+    private int actualLevel;
+
+    /**
+     * Hauteur en mètres à laquelle il est situé
+     */
+    private double actualHeight;
+
+    /**
+     * Liste des prochains niveaux à se déplacer
+     */
+    private ArrayList<Integer> nextLevels;
+
+    /**
+     * Arrêt d'urgence
+     */
+    private boolean isEmergencyStopped;
+
+    /**
+     * Constructeur par défaut
+     */
+    public Moteur(int levels, int defaultLevel) {
+        this.levels = levels;
+        this.actualLevel = defaultLevel;
+        this.actualHeight = getHeightFromLevel(defaultLevel);
+        this.nextLevels = new ArrayList<>();
+        this.isEmergencyStopped = false;
+    }
+
+    /**
+     * Retourne la hauteur en mètres à partir d'un niveau
+     * @param level niveau
+     * @return hauteur en mètres
+     */
+    private double getHeightFromLevel(int level) {
+        //La cabine de l'ascenseur fait 2 mètres de haut,
+        //entre chaque niveau il y a une marge de 40cm
+        return 2.4*level;
+    }
+
+    /*** GETTERS & SETTERS ***/
+
+    public int getActualLevel() {
+        return actualLevel;
+    }
+
+    public void setActualLevel(int actualLevel) {
+        this.actualLevel = actualLevel;
+    }
+
+    public double getActualHeight() {
+        return actualHeight;
+    }
+
+    public void setActualHeight(double actualHeight) {
+        this.actualHeight = actualHeight;
+    }
+
+    public boolean isEmergencyStopped() {
+        return isEmergencyStopped;
+    }
+
+    public void setEmergencyStopped(boolean emergencyStopped) {
+        isEmergencyStopped = emergencyStopped;
+    }
+}
