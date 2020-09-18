2306dfffe120094670d706f0f505b6c6144d1262
Quentin Pla <quentin-pla@hotmail.fr>
2020-09-18 18:11:23 +0200

    Implémentation des controleurs

---
+++ src/main/java/controllers/ControleurMoteur.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import models.Moteur;
+
+public class ControleurMoteur {
+
+    /**
+     * Moteur
+     */
+    private Moteur engine;
+
+    /**
+     * Constructeur par défaut
+     * @param engine
+     */
+    public ControleurMoteur(Moteur engine) {
+        this.engine = engine;
+    }
+}
